cdd63972db4ede22ffe2d00c5af25d442c393841
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-12-16 22:37:09 +0530

    chore: :wrench: update head meta config

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col font-fira-code" >
-      <h1>
-        Hello
-      </h1>
-    </main>
-  )
+    return (
+        <main className="min-h-screen flex flex-col font-fira-code">
+            <h1>
+                Hello
+            </h1>
+        </main>
+    );
 }
